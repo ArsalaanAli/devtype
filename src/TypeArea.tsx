c677fa0c26f93aa3ff45517f6d87f7bc5ee8449f
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2023-02-16 23:21:31 -0500

    restarting :/

--- src/TypeArea.tsx
+++ src/TypeArea.tsx
@@ -1,15 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function TypeArea() {
-  const [typingText, setTypingText] = useState(
-    "The quick brown fox jumps over the lazy dog"
-  );
-
-  return (
-    <div>
-      <h1 className="flex items-center justify-center h-96">{typingText}</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default TypeArea;
